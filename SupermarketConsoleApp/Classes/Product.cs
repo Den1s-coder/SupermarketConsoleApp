caee95c0841b773c3d27cfb0ec83389b0ea0433c
Denis Katasonov <diarusfoxs@gmail.com>
2024-10-09 16:16:22 +0300

    Added Methods: Create and GetName

--- SupermarketConsoleApp/Classes/Product.cs
+++ SupermarketConsoleApp/Classes/Product.cs
@@ -8,5 +8,38 @@ namespace SupermarketConsoleApp.Classes
 {
     internal class Product
     {
+        string Name;
+        int ProductId;
+        double Price;
+
+        private Product(string name, int productId, double price)
+        {
+            Name = name;
+            ProductId = productId;
+            Price = price;
+        }
+
+        public static void CreateProduct(List<Product> Products, string name, int productId, double price)
+        {
+            if(string.IsNullOrEmpty(name))
+            {
+                Console.WriteLine("Помилка: Назва продукту не може бути пустою");
+                return;
+            }
+
+            if (Products.Any(p => p.Name == name))
+            {
+                Console.WriteLine($"Помилка: Товар {name} вже iснує ");
+                return;
+            }
+
+            Products.Add(new Product(name, productId, price));
+            return;
+        }
+
+        public void GetName()
+        {
+            Console.WriteLine($"Name: {Name}");
+        }
     }
 }
