16a7a7c53457d85ba9a8048db158b0c6734cd43f
Denis Katasonov <diarusfoxs@gmail.com>
2024-10-10 17:10:59 +0300

    Update Add Purshase and Count Methods

--- SupermarketConsoleApp/Classes/CashRegister.cs
+++ SupermarketConsoleApp/Classes/CashRegister.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using SupermarketConsoleApp.Payments.Interface;
+using SupermarketConsoleApp.Payments;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -9,46 +11,101 @@ namespace SupermarketConsoleApp.Classes
     internal class CashRegister
     {
         int Id;
-        double Amount;
+        double Amount = 1000;
         List<string> Log;
         string Model;
+        List<Product> Products;
 
 
-        private CashRegister(int id,double amount, string model)
+        private CashRegister(int id, string model, List<Product> products)
         {
             Id = id;
-            Amount = amount;
             Model = model;
+            Products = products;
         }
 
-        public CashRegister CreateCashRegister(int id, double amount, string model)    
+        public static CashRegister CreateCashRegister(int id, string model, List<Product> products)    
         {
             if (string.IsNullOrEmpty(model))
             {
                 Console.WriteLine("Помилка: назва моделі не може бути пустою");
                 return null;
             }
-            return new CashRegister(id, amount, model);
+            return new CashRegister(id, model, products);
         }
 
-        public void withdraw(double value) 
+        public void Withdraw(double value) 
         {
-            if(Amount <= 0)
-            {
-                Amount = 0;
-                Console.WriteLine("Увага на касі закінчились гроші!");
-                return;
-            }
+            if (value < 0) value *= -1; 
+
             if (value > Amount) 
             {
                 Console.WriteLine("Помилка: на касі не достатньо грошей!");
                 return;
             }
-            if(value < 0) {value*=-1;}
 
             Amount -= value;
-            Console.WriteLine($"Готівка успішно знята \nЗалишилось готівки - {Amount}");
+            Console.WriteLine($"Готівка успішно знята \nЗалишилось готівки на кассі - {Amount}");
             return;
+        }
+
+        public void Purchase(double amount)
+        {
+            Console.WriteLine("Выберите метод оплаты (1 - Кредитная карта, 2 - Готівка):");
+            string choice = Console.ReadLine();
+            if (choice != "1" && choice != "2") 
+            {
+                Console.WriteLine("Помилка: Неправильний вибір. Спробуйте ще раз.");
+                return;
+            }
+
+            IPaymentForm paymentsChoice = choice switch
+            {
+                "1" => new CreditCardPayment(),
+                "2" => new CashPayment(this)
+            };
+
+            PaymentProcessor paymentProcessor = new PaymentProcessor(paymentsChoice);
+            paymentProcessor.ProcessPayment(amount);
+
+            return;
+        }
+
+        public double Count()
+        {
+            double amount = 0;
+
+            for (int i = 0; i < Products.Count; i++) Products[i].GetName();
+
+            while (true)
+            {
+                Console.WriteLine("Вкажіть Id товару який хочете взяти: ");
+                int ChoiseId;
+
+                while (!int.TryParse(Console.ReadLine(), out ChoiseId))
+                {
+                    Console.WriteLine("Помилка: Невірний формат введення спробуйте ще раз:");
+                };
+
+                amount += Products[ChoiseId].GetPrice();
+                Console.WriteLine("Хочете додати щось ще?");
+
+                while (!int.TryParse(Console.ReadLine(), out ChoiseId))
+                {
+                    Console.WriteLine("Помилка: Невірний формат введення спробуйте ще раз:");
+                };
+
+                switch (ChoiseId)
+                {
+                    case 1: break;
+                    case 2: return amount;
+                }
+            }
+        }
+
+        public void CheckGenerator(int id) 
+        {
+
         }
     }
 }
