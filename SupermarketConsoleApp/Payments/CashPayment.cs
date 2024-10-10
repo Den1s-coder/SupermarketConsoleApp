02991af8b31b809c41faccbf6051a7d6cede7c0d
Denis Katasonov <diarusfoxs@gmail.com>
2024-10-10 17:18:53 +0300

    Add CashPayment class for cash payments operations

---
+++ SupermarketConsoleApp/Payments/CashPayment.cs
@@ -0,0 +1,27 @@
+﻿using SupermarketConsoleApp.Classes;
+using SupermarketConsoleApp.Payments.Interface;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SupermarketConsoleApp.Payments
+{
+    internal class CashPayment : IPaymentForm
+    {
+        CashRegister CashRegister;
+
+        public CashPayment(CashRegister cashRegister)
+        {
+            CashRegister = cashRegister;
+        }
+
+        public void ProcessPayment(double amount)
+        {
+            CashRegister.Withdraw(amount);
+
+            Console.WriteLine($"Оплата готiвкою на сумму {amount} успiшна");
+        }
+    }
+}
