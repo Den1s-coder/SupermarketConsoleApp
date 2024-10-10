fd832ab87e28306d231e338956a7146098e7aa65
Denis Katasonov <diarusfoxs@gmail.com>
2024-10-10 17:22:02 +0300

    Add PaymantProcesing class for Paymant processing

---
+++ SupermarketConsoleApp/Payments/PaymentProcessor.cs
@@ -0,0 +1,24 @@
+﻿using SupermarketConsoleApp.Payments.Interface;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SupermarketConsoleApp.Payments
+{
+    internal class PaymentProcessor
+    {
+        IPaymentForm PaymentForm;
+
+        public PaymentProcessor(IPaymentForm paymentForm)
+        {
+            PaymentForm = paymentForm;
+        }
+
+        public void ProcessPayment(double amount) 
+        {
+            PaymentForm.ProcessPayment(amount);
+        }
+    }
+}
