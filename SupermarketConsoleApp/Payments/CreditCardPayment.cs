ee720eb874efef9dc5fe6b7d68ebc939b6f91a4d
Denis Katasonov <diarusfoxs@gmail.com>
2024-10-10 17:19:52 +0300

    Add CreditCardPayment class for CreditCard payments operations

---
+++ SupermarketConsoleApp/Payments/CreditCardPayment.cs
@@ -0,0 +1,18 @@
+﻿using SupermarketConsoleApp.Payments.Interface;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SupermarketConsoleApp.Payments
+{
+    internal class CreditCardPayment : IPaymentForm
+    {
+        public void ProcessPayment(double amount)
+        {
+
+            Console.WriteLine($"Оплата карткою на сумму {amount} успiшна!");
+        }
+    }
+}
