f5641a5e30638b5f8cb6fecd5e092271b9be5219
Denis Katasonov <diarusfoxs@gmail.com>
2024-10-10 17:14:24 +0300

    Add Interface directory and IPaymentForm Interface

---
+++ SupermarketConsoleApp/Payments/Interface/IPaymentForm.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SupermarketConsoleApp.Payments.Interface
+{
+    internal interface IPaymentForm
+    {
+        void ProcessPayment(double amount);
+    }
+}
