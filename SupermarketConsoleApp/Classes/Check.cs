db3e36726d44058a6cfbf9db35e1179499bf475b
Denis Katasonov <diarusfoxs@gmail.com>
2024-10-10 17:12:44 +0300

    Add Check class

---
+++ SupermarketConsoleApp/Classes/Check.cs
@@ -0,0 +1,17 @@
+﻿using SupermarketConsoleApp.Payments.Interface;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SupermarketConsoleApp.Classes
+{
+    class Check
+    {
+        DateTime Time = DateTime.Now;
+        IPaymentForm paymentForm;
+
+    }
+}
