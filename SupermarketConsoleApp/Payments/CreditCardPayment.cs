2ab37408d61d4118de45be628418bd1a542bc37d
Denis Katasonov <diarusfoxs@gmail.com>
2024-10-23 19:02:41 +0300

    Add Count Method

--- SupermarketConsoleApp/Payments/CreditCardPayment.cs
+++ SupermarketConsoleApp/Payments/CreditCardPayment.cs
@@ -1,4 +1,5 @@
-﻿using SupermarketConsoleApp.Payments.Interface;
+﻿using SupermarketConsoleApp.Classes;
+using SupermarketConsoleApp.Payments.Interface;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -7,9 +8,38 @@ using System.Threading.Tasks;
 
 namespace SupermarketConsoleApp.Payments
 {
-    internal class CreditCardPayment : IPaymentForm
+    internal struct CreditCardPayment : IPaymentForm
     {
-        public void ProcessPayment(double amount)
+        CashRegister CashRegister;
+
+        double Amount;
+
+        double Paid;
+
+        bool LoyaltyCard = false;
+
+        bool CourierDelivery = false;
+
+        public CreditCardPayment(CashRegister cashRegister)
+        {
+            CashRegister = cashRegister;
+        }
+
+        public void Count(List<Product> Basket)
+        {
+            foreach (var product in Basket)
+            {
+                Amount += product.GetPrice();
+            }
+
+            if (LoyaltyCard == true) Amount -= Amount * 0.05;
+
+            if (CourierDelivery == true) Amount += 250;
+
+            ProcessPayment(Amount);
+        }
+
+        private void ProcessPayment(double amount)
         {
 
             Console.WriteLine($"Оплата карткою на сумму {amount} успiшна!");
