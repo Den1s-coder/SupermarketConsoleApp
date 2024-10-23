601fc6f7450e3a6ab76d81a37d992e33f9941233
Denis Katasonov <diarusfoxs@gmail.com>
2024-10-23 19:01:35 +0300

    Add CourierPayments class

---
+++ SupermarketConsoleApp/Payments/СourierPayment.cs
@@ -0,0 +1,45 @@
+﻿using SupermarketConsoleApp.Classes;
+using SupermarketConsoleApp.Payments.Interface;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SupermarketConsoleApp.Payments
+{
+    internal struct СourierPayment : IPaymentForm
+    {
+        CashRegister CashRegister;
+
+        double Amount;
+
+        bool LoyaltyCard = false;
+
+        bool CourierDelivery = false;
+
+        public СourierPayment(CashRegister cashRegister)
+        {
+            CashRegister = cashRegister;
+        }
+
+        public void Count(List<Product> Basket)
+        {
+            foreach (var product in Basket)
+            {
+                Amount += product.GetPrice();
+            }
+
+            if (LoyaltyCard == true) Amount -= Amount * 0.05;
+
+            if (CourierDelivery == true) Amount += 250;
+
+            ProcessPayment(Amount);
+        }
+
+        private void ProcessPayment(double amount)
+        {
+            Console.WriteLine($"Оплата кур'єру на сумму {amount} успiшна!");
+        }
+    }
+}
