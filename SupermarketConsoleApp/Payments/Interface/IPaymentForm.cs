a28676a52240ad125e254278b2e9820ff3d46c91
Denis Katasonov <diarusfoxs@gmail.com>
2024-10-23 19:03:44 +0300

    Update Count method

--- SupermarketConsoleApp/Payments/Interface/IPaymentForm.cs
+++ SupermarketConsoleApp/Payments/Interface/IPaymentForm.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using SupermarketConsoleApp.Classes;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,6 +9,6 @@ namespace SupermarketConsoleApp.Payments.Interface
 {
     internal interface IPaymentForm
     {
-        void ProcessPayment(double amount);
+        void Count(List<Product> Basket);
     }
 }
