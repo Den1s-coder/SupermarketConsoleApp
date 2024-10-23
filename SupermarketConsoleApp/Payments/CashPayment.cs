228e52a4baefb83020610f1f4621a16407e8ea49
Denis Katasonov <diarusfoxs@gmail.com>
2024-10-23 19:02:23 +0300

    Add Count Method

--- SupermarketConsoleApp/Payments/CashPayment.cs
+++ SupermarketConsoleApp/Payments/CashPayment.cs
@@ -8,18 +8,44 @@ using System.Threading.Tasks;
 
 namespace SupermarketConsoleApp.Payments
 {
-    internal class CashPayment : IPaymentForm
+    internal struct CashPayment : IPaymentForm
     {
         CashRegister CashRegister;
+
+        double Amount;
+
+        double Paid;
+
+        double Rest;
+
+        bool LoyaltyCard = false;
+
+        bool CourierDelivery = false;
 
         public CashPayment(CashRegister cashRegister)
         {
             CashRegister = cashRegister;
         }
 
-        public void ProcessPayment(double amount)
+        public void Count(List<Product> Basket)
+        {
+            foreach (var product in Basket) 
+            {  
+                Amount += product.GetPrice();
+            }
+
+            if (LoyaltyCard == true) Amount -= Amount * 0.05;
+
+            if (CourierDelivery == true) Amount += 250;
+
+            ProcessPayment(Amount);
+        }
+
+        private void ProcessPayment(double amount)
         {
             CashRegister.Withdraw(amount);
+
+            Check.CheckGenerator(CashRegister, amount, "готiвка");
 
             Console.WriteLine($"Оплата готiвкою на сумму {amount} успiшна");
         }
