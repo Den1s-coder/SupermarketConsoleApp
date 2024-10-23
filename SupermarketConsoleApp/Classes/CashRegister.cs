3abb7a9bb626a5edcdfa0ec88df37e06bd882ec8
Denis Katasonov <diarusfoxs@gmail.com>
2024-10-23 18:53:10 +0300

    Add WitdrawCash,AddCash,Busket,GetLogger,Getid Methods

--- SupermarketConsoleApp/Classes/CashRegister.cs
+++ SupermarketConsoleApp/Classes/CashRegister.cs
@@ -5,6 +5,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Linq;
 
 namespace SupermarketConsoleApp.Classes
 {
@@ -12,7 +13,7 @@ namespace SupermarketConsoleApp.Classes
     {
         int Id;
         double Amount = 1000;
-        List<string> Log;
+        List<Check> Log;
         string Model;
         List<Product> Products;
 
@@ -22,9 +23,10 @@ namespace SupermarketConsoleApp.Classes
             Id = id;
             Model = model;
             Products = products;
+            Log = new List<Check>();
         }
 
-        public static CashRegister CreateCashRegister(int id, string model, List<Product> products)    
+        public static CashRegister CreateCashRegister(int id, string model, List<Product> products)
         {
             if (string.IsNullOrEmpty(model))
             {
@@ -34,11 +36,11 @@ namespace SupermarketConsoleApp.Classes
             return new CashRegister(id, model, products);
         }
 
-        public void Withdraw(double value) 
+        public void Withdraw(double value)
         {
-            if (value < 0) value *= -1; 
+            if (value < 0) value *= -1;
 
-            if (value > Amount) 
+            if (value > Amount)
             {
                 Console.WriteLine("Помилка: на касі не достатньо грошей!");
                 return;
@@ -49,11 +51,44 @@ namespace SupermarketConsoleApp.Classes
             return;
         }
 
-        public void Purchase(double amount)
+        public void WithdrawCash()
         {
-            Console.WriteLine("Выберите метод оплаты (1 - Кредитная карта, 2 - Готівка):");
+            Console.WriteLine("Вкажiть сумму яку хочете зняти з касси");
+            int value;
+
+            while (!int.TryParse(Console.ReadLine(), out value))
+            {
+                Console.WriteLine("Помилка: Невірний формат введення спробуйте ще раз:");
+            };
+
+            Withdraw(value);
+            
+            return;
+        }
+
+        public void AddCash()
+        {
+            Console.WriteLine("Вкажiть сумму на яку хочете поповнити кассу");
+            int value;
+
+            while (!int.TryParse(Console.ReadLine(), out value))
+            {
+                Console.WriteLine("Помилка: Невірний формат введення спробуйте ще раз:");
+            };
+
+            if (value < 0) value *= -1;
+
+            Amount += value;
+            Console.WriteLine($"Поповнення Успішне на кассі = {Amount} грн");
+            return;
+        }
+
+        private void Purchase(List<Product> Basket)
+        {
+            Console.WriteLine("Выберите метод оплаты (1 - Кредитная карта, 2 - Готівка, 3 - Кур'єр):");
             string choice = Console.ReadLine();
-            if (choice != "1" && choice != "2") 
+
+            if (choice != "1" && choice != "2" && choice != "3")
             {
                 Console.WriteLine("Помилка: Неправильний вибір. Спробуйте ще раз.");
                 return;
@@ -61,22 +96,25 @@ namespace SupermarketConsoleApp.Classes
 
             IPaymentForm paymentsChoice = choice switch
             {
-                "1" => new CreditCardPayment(),
-                "2" => new CashPayment(this)
+                "1" => new CreditCardPayment(this),
+                "2" => new CashPayment(this),
+                "3" => new СourierPayment(this)
             };
 
             PaymentProcessor paymentProcessor = new PaymentProcessor(paymentsChoice);
-            paymentProcessor.ProcessPayment(amount);
+            paymentProcessor.ProcessPayment(Basket);
 
             return;
         }
 
-        public double Count()
+        public void Basket()
         {
+            List<Product> Basket = new List<Product>();
+
             double amount = 0;
 
-            for (int i = 0; i < Products.Count; i++) Products[i].GetName();
-
+            for (int i = 0; i < Products.Count; i++) Products[i].GetInfo();
+            Console.WriteLine($"Вкажiть {Products.Count + 1} якщо хочете вийти");
             while (true)
             {
                 Console.WriteLine("Вкажіть Id товару який хочете взяти: ");
@@ -84,11 +122,26 @@ namespace SupermarketConsoleApp.Classes
 
                 while (!int.TryParse(Console.ReadLine(), out ChoiseId))
                 {
-                    Console.WriteLine("Помилка: Невірний формат введення спробуйте ще раз:");
+                    Console.WriteLine("Помилка: Невiрний формат введення спробуйте ще раз:");
                 };
 
-                amount += Products[ChoiseId].GetPrice();
-                Console.WriteLine("Хочете додати щось ще?");
+                if (ChoiseId == Products.Count + 1) return;
+
+                if (!Products.Any(p => p.GetId() == ChoiseId)) 
+                {
+                    Console.WriteLine("Помилка: Товару з таким ID не iснує:");
+                    break;
+                }
+
+                if (Basket.Any(p => p.GetId() == ChoiseId))
+                {
+                    Product product = Basket.First(p => p.GetId() == ChoiseId);
+                    product.AddQuantity();
+                }
+                else Basket.Add(Products[ChoiseId]);
+
+
+                Console.WriteLine("Хочете додати щось ще? (1(Так)/2(Ні)");
 
                 while (!int.TryParse(Console.ReadLine(), out ChoiseId))
                 {
@@ -97,15 +150,20 @@ namespace SupermarketConsoleApp.Classes
 
                 switch (ChoiseId)
                 {
-                    case 1: break;
-                    case 2: return amount;
+                    case 1:
+                        break;
+                    case 2:
+                        this.Purchase(Basket);
+                        break;
+                    default:
+                        Console.WriteLine("Помилка: Неправильний вибір. Спробуйте ще раз.");
+                        break;
                 }
             }
         }
 
-        public void CheckGenerator(int id) 
-        {
+        public List<Check> GetLogger() { return Log; }
 
-        }
+        public int GetId() {  return Id; }
     }
 }
